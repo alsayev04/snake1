6a4afc9dcf5e3445baf51cc525bc30ec2d3a02dd
alsayev04 <121810378+alsayev04@users.noreply.github.com>
2026-06-07 23:30:46 +0800

    for

--- snake.py
+++ snake.py
@@ -1,2 +1,17 @@
-ghjcghjcghjcjcghjcghj
-ghjcghjghjcgjcghjcghj
+import pygame
+import time
+import random
+
+# Инициализация pygame
+pygame.init()
+
+#Цвета (RGB)
+white = (255,255,255)
+black = (0,0,0)
+red = (213,50,80)
+green = (0,255,0)
+blue = (50,153,213)
+
+#Размеры экрана
+dis_width = 800
+dis_height = 600
